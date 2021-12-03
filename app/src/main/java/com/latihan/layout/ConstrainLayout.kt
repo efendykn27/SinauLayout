3b3d2f791ff46f0c384b2ee4b87c94b05d91f9ec
efendykn27 <efendikn27@gmail.com>
2021-12-03 16:06:23 +0700

    Layout Sinau

---
+++ app/src/main/java/com/latihan/layout/ConstrainLayout.kt
@@ -0,0 +1,11 @@
+package com.latihan.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstrainLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrain_layout)
+    }
+}
---
+++ app/src/main/java/com/latihan/layout/frameLayout.kt
@@ -0,0 +1,11 @@
+package com.latihan.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class frameLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
